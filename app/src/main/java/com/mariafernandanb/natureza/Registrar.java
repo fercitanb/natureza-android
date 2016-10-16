49f509007e4383e22ac88732d064cfb65ca4bab8
fercitanb <mfndpb@gmail.com>
2016-10-16 11:27:58 -0400

    first commit

---
+++ app/src/main/java/com/mariafernandanb/natureza/Registrar.java
@@ -0,0 +1,196 @@
+package com.mariafernandanb.natureza;
+
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+
+import com.mariafernandanb.natureza.util.Constantes;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Registrar extends AppCompatActivity {
+
+    private AutoCompleteTextView mciv, mnombrev, mappatv, mapmat, mnumv, memailv;
+    private Button mregistrarv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrar);
+
+        mciv = (AutoCompleteTextView) findViewById(R.id.ci);
+        mnombrev = (AutoCompleteTextView) findViewById(R.id.nombre);
+        mappatv = (AutoCompleteTextView) findViewById(R.id.apPat);
+        mapmat = (AutoCompleteTextView) findViewById(R.id.apMat);
+        mnumv = (AutoCompleteTextView) findViewById(R.id.num);
+        memailv = (AutoCompleteTextView) findViewById(R.id.correo);
+        mregistrarv = (Button) findViewById(R.id.registrar);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mregistrarv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String ci = mciv.getText().toString();
+                String nom = mnombrev.getText().toString();
+                String ap = mappatv.getText().toString();
+                String am = mapmat.getText().toString();
+                String num = mnumv.getText().toString();
+                String corr = memailv.getText().toString();
+
+                new RegistroUsuario(ci,nom,ap,am,num,corr).execute();
+            }
+        });
+
+    }
+
+    public class RegistroUsuario extends AsyncTask<Void, Void, Void> {
+
+        HttpURLConnection httpURLConnection;
+        StringBuilder result;
+        URL url;
+        private final String mCi;
+        private final String mNombre;
+        private final String mApPat;
+        private final String mApMat;
+        private final String mNum;
+        private final String mCorreo;
+
+        RegistroUsuario(String mCi, String mNombre, String mApPat, String mApMat, String mNum, String mCorreo) {
+            this.mNum = mNum;
+            this.mCorreo = mCorreo;
+            this.mCi = mCi;
+            this.mNombre = mNombre;
+            this.mApPat = mApPat;
+            this.mApMat = mApMat;
+
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            // TODO: register the new account here.
+            try {
+                url = new URL(Constantes._URL_REGISTRO);
+            } catch (MalformedURLException e){
+                e.printStackTrace();
+            }
+
+            try {
+                httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.setReadTimeout(Constantes.READ_TIMEOUT);
+                httpURLConnection.setConnectTimeout(Constantes.CONNECTION_TIMEOUT);
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoInput(true);
+                httpURLConnection.setDoOutput(true);
+
+                Uri.Builder builder = new Uri.Builder()
+                        .appendQueryParameter("ci", mCi).appendQueryParameter("nombre", mNombre).appendQueryParameter("apellidoPaterno", mApPat).appendQueryParameter("apellidoMaterno", mApMat).appendQueryParameter("numeroTelefono", mNum).appendQueryParameter("email", mCorreo);
+
+                String request = builder.build().getEncodedQuery();
+
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+                bufferedWriter.write(request);
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                outputStream.close();
+                httpURLConnection.connect();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                int response_code = httpURLConnection.getResponseCode();
+
+                if (response_code == HttpURLConnection.HTTP_OK) {
+                    InputStream inputStream = httpURLConnection.getInputStream();
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+
+                    result = new StringBuilder();
+                    String line;
+
+                    while ((line = bufferedReader.readLine()) != null) {
+                        result.append(line);
+                    }
+
+                } else {
+
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void success) {
+
+
+            try {
+                JSONObject group_info = new JSONObject(String.valueOf(result));
+
+                JSONArray code = group_info.getJSONArray("code");
+
+                for (int i = 0; i < code.length() ; i++) {
+                    int response = code.getInt(i);
+
+
+                    switch (response){
+                        case 0:
+                            Log.d("Registro","CLIENTE EXISTENTE: " + response);
+                            break;
+                        case 1:
+                            Log.d("Registro", "REGISTO EXITOSO: " + response);
+                            break;
+                        case 2:
+                            Log.d("Registro", "ERROR AL ENVIAR CORREO COMUNICARSE CON LA EMPRESA: " + response);
+                            break;
+                    }
+                    /*if (response == 4) {
+                        Toast.makeText(context, "Sanción guardada exitosamente!", Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(context, "Error, Verifique su CI o CODIGO SECRETO", Toast.LENGTH_LONG).show();
+                    }*/
+
+                    Log.d("Response", "value: " + response);
+
+                }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            /*if (success) {
+                finish();
+            } else {
+                mPasswordView.setError(getString(R.string.error_incorrect_password));
+                mPasswordView.requestFocus();
+            }*/
+        }
+
+    }
+
+}
---
+++ app/src/main/java/com/mariafernandanb/natureza/util/Constantes.java
@@ -0,0 +1,15 @@
+package com.mariafernandanb.natureza.util;
+
+/**
+ * Created by HUGO on 09/10/2016.
+ */
+public class Constantes {
+
+    public static final int CONNECTION_TIMEOUT = 10000;
+    public static final int READ_TIMEOUT = 15000;
+
+    // URL from Services
+    //public static final
+    public static final String _URL_AUTENTIFICACION = "http://192.168.1.44:90/natureza/WebServices/autentificacion.php";
+    public static final String _URL_REGISTRO = "http://192.168.1.44:90/natureza/WebServices/registroCliente.php";
+}
